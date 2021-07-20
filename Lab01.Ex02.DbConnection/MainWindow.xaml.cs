e1f9e8883ec5083bbdda170984f6ff008b2b6b59
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-20 22:09:03 +0300

    ful lab01 ex02

--- Lab01.Ex02.DbConnection/MainWindow.xaml.cs
+++ Lab01.Ex02.DbConnection/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,41 @@ namespace Lab01.Ex02.DbConnection
     /// </summary>
     public partial class MainWindow : Window
     {
+        OleDbConnection connection = new OleDbConnection();
+        string testConnect = @"Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=ApressFinancial;Data Source=USER-ПК";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItemConnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = testConnect;
+                    connection.Open();
+                    MessageBox.Show("Соединение с базой данных выполнено успешно");
+                }
+                else
+                    MessageBox.Show("Соединение с базой данных уже установлено");
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка соединения с базой данных");
+            }
+
+        }
+        private void MenuItemDisConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                MessageBox.Show("Соединение с базой данных закрыто");
+            }
+            else
+                MessageBox.Show("Соединение с базой данных уже закрыто");
         }
     }
 }
