e7022d3c72ff037596dfc65ef3e816989960e5f4
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-22 22:06:57 +0300

    ful lab02 ex01

--- Lab02.Ex01.CommandsDB/Form1.Designer.cs
+++ Lab02.Ex01.CommandsDB/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.MenuItemConnect = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuItemDisConnect = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuItemListConnect = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -77,11 +79,32 @@
             this.MenuItemListConnect.Text = "Список соединений";
             this.MenuItemListConnect.Click += new System.EventHandler(this.MenuItemListConnect_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Список продуктов";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Список продуктов";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -100,6 +123,8 @@
         private System.Windows.Forms.ToolStripMenuItem MenuItemConnect;
         private System.Windows.Forms.ToolStripMenuItem MenuItemDisConnect;
         private System.Windows.Forms.ToolStripMenuItem MenuItemListConnect;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
