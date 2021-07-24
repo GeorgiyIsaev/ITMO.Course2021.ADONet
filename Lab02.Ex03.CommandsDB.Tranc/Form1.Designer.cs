8cc169f5fa55e5dbb2230374fc2347e4e7d379b6
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-24 21:38:12 +0300

    full copy lab02 ex03

---
+++ Lab02.Ex03.CommandsDB.Tranc/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Lab02.Ex01.CommandsDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.соединениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemConnect = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemDisConnect = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemListConnect = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.соединениеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // соединениеToolStripMenuItem
+            // 
+            this.соединениеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemConnect,
+            this.MenuItemDisConnect,
+            this.MenuItemListConnect});
+            this.соединениеToolStripMenuItem.Name = "соединениеToolStripMenuItem";
+            this.соединениеToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.соединениеToolStripMenuItem.Text = "Соединение";
+            // 
+            // MenuItemConnect
+            // 
+            this.MenuItemConnect.Name = "MenuItemConnect";
+            this.MenuItemConnect.Size = new System.Drawing.Size(184, 22);
+            this.MenuItemConnect.Text = "Подключить";
+            this.MenuItemConnect.Click += new System.EventHandler(this.MenuItemConnect_Click);
+            // 
+            // MenuItemDisConnect
+            // 
+            this.MenuItemDisConnect.Name = "MenuItemDisConnect";
+            this.MenuItemDisConnect.Size = new System.Drawing.Size(184, 22);
+            this.MenuItemDisConnect.Text = "Отключить";
+            this.MenuItemDisConnect.Click += new System.EventHandler(this.MenuItemDisConnect_Click);
+            // 
+            // MenuItemListConnect
+            // 
+            this.MenuItemListConnect.Name = "MenuItemListConnect";
+            this.MenuItemListConnect.Size = new System.Drawing.Size(184, 22);
+            this.MenuItemListConnect.Text = "Список соединений";
+            this.MenuItemListConnect.Click += new System.EventHandler(this.MenuItemListConnect_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Список продуктов";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Список продуктов";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(16, 169);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(205, 170);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Название продукта";
+            this.columnHeader1.Width = 123;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(205, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Список продуктов";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem соединениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemConnect;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemDisConnect;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemListConnect;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Lab02.Ex03.CommandsDB.Tranc/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab02.Ex01.CommandsDB
+{
+  
+    public partial class Form1 : Form
+    {
+        OleDbConnection connection = new OleDbConnection();
+        // string testConnect = @"Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=ApressFinancial;Data Source=USER-ПК";
+        string testConnect = GetConnectionStringByName("DBConnect.NorthwindConnectionString");
+        public Form1()
+        {
+            InitializeComponent();
+            this.connection.StateChange += new
+                System.Data.StateChangeEventHandler(
+                this.connection_StateChange);
+        }
+   
+        private void connection_StateChange(object sender, System.Data.StateChangeEventArgs e)
+        {
+            MenuItemConnect.Enabled =
+                (e.CurrentState == ConnectionState.Closed);
+            MenuItemDisConnect.Enabled =
+                (e.CurrentState == ConnectionState.Open);
+        }
+        static string GetConnectionStringByName(string name)
+        {
+            string returnValue = null;
+            ConnectionStringSettings settings =
+                ConfigurationManager.ConnectionStrings[name];
+            if (settings != null)
+                returnValue = settings.ConnectionString;
+            return returnValue;
+        }
+
+
+        private void MenuItemConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = testConnect;
+                    connection.Open();
+                    MessageBox.Show("Соединение с базой данных выполнено успешно");
+                }
+                else
+                    MessageBox.Show("Соединение с базой данных уже установлено");
+            }
+            catch (OleDbException XcpSQL)
+            {
+                foreach (OleDbError se in XcpSQL.Errors)
+                {
+                    MessageBox.Show(se.Message,
+                    "SQL Error code " + se.NativeError,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception Xcp)
+            {
+                MessageBox.Show(Xcp.Message, "Unexpected Exception",
+                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MenuItemDisConnect_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                MessageBox.Show("Соединение с базой данных закрыто");
+            }
+            else
+                MessageBox.Show("Соединение с базой данных уже закрыто");
+        }
+
+        private void MenuItemListConnect_Click(object sender, EventArgs e)
+        {
+            ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
+            if (settings != null)
+            {
+                foreach (ConnectionStringSettings cs in settings)
+                {
+                    MessageBox.Show("name = " + cs.Name);
+                    MessageBox.Show("providerName = " + cs.ProviderName);
+                    MessageBox.Show("connectionString = " + cs.ConnectionString);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                MessageBox.Show("Сначала подключитесь к базе");
+                return;
+            }
+            OleDbCommand command = new OleDbCommand();
+
+            command.Connection = connection;
+            command.CommandText = "SELECT COUNT(*) FROM Products";
+            int number = (int)command.ExecuteScalar();
+            label1.Text = number.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                MessageBox.Show("Сначала подключитесь к базе");
+                return;
+            }
+            OleDbCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT ProductName FROM Products";
+            OleDbDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                listView1.Items.Add(reader["ProductName"].ToString());
+            }
+        }
+    }
+}
