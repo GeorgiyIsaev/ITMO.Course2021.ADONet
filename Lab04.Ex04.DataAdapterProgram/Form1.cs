4e16767e2e2a674c798dfcd3e680afbac6cc0598
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-29 23:10:35 +0300

    full lab04 ex04

--- Lab04.Ex04.DataAdapterProgram/Form1.cs
+++ Lab04.Ex04.DataAdapterProgram/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace Lab04.Ex04.DataAdapterProgram
 {
@@ -15,6 +17,22 @@ namespace Lab04.Ex04.DataAdapterProgram
         public Form1()
         {
             InitializeComponent();
+        }
+        private SqlConnection NorthwindConnection = new SqlConnection("Data Source=(local);Initial Catalog=Northwind;Integrated Security=True");
+        private SqlDataAdapter SqlDataAdapter1;
+        private DataSet NorthwindDataset = new DataSet("Northwind");
+        private DataTable CustomersTable = new DataTable("Customers");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter1 = new SqlDataAdapter("SELECT * FROM Customers", NorthwindConnection);
+            NorthwindDataset.Tables.Add(CustomersTable);
+            SqlDataAdapter1.Fill(NorthwindDataset.Tables["Customers"]);
+            dataGridView1.DataSource = NorthwindDataset.Tables["Customers"];
+            SqlCommandBuilder commands = new SqlCommandBuilder(SqlDataAdapter1);
+            NorthwindDataset.EndInit();
+            SqlDataAdapter1.Update(NorthwindDataset.Tables["Customers"]);
+
         }
     }
 }
