0e3e24df85b9c00bcdb7f66c925d89d0d0c61a43
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-30 22:17:28 +0300

    create lab07 ex02

---
+++ Lab07.Ex02.LinqStudent/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07.Ex02.LinqStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
