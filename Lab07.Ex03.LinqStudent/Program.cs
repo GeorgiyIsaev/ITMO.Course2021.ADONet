b03f92c568bd26429842a3d41278fa088d8ce052
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-30 22:25:56 +0300

    full lab07 ex03

---
+++ Lab07.Ex03.LinqStudent/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07.Ex03.LinqStudent
+{
+    class Program
+    {
+        static List<Student> students = new List<Student>
+        {
+           new Student {First="Svetlana", Last="Omelchenko", ID=111, Scores= new List<int> {97, 92, 81, 60}},
+           new Student {First="Claire", Last="O’Donnell", ID=112, Scores= new List<int> {75, 84, 91, 39}},
+        };
+
+        static void Main(string[] args)
+        {
+            IEnumerable<Student> studentQuery =
+                from student in students
+                where student.Scores[0] > 90 && student.Scores[3] < 80
+                //orderby student.Last ascending
+                orderby student.Scores[0] descending
+                select student;
+            foreach (Student student in studentQuery)
+            {
+                Console.WriteLine("{0}, {1} {2}", student.Last, student.First, student.Scores[0]);
+            }
+            Console.WriteLine();
+
+            var studentQuery2 =
+                from student in students
+                group student by student.Last[0];
+            foreach (var studentGroup in studentQuery2)
+            {
+                Console.WriteLine(studentGroup.Key);
+                foreach (Student student in studentGroup)
+                {
+                    Console.WriteLine("   {0}, {1}",
+                              student.Last, student.First);
+                }
+            }
+
+            /*Присвоение переменной неявного типа*/
+            var studentQuery3 =
+                from student in students
+                group student by student.Last[0];
+
+            foreach (var groupOfStudents in studentQuery3)
+            {
+                Console.WriteLine(groupOfStudents.Key);
+                foreach (var student in groupOfStudents)
+                {
+                    Console.WriteLine("   {0}, {1}",
+                        student.Last, student.First);
+                }
+            }
+
+            /*Упорядочение групп по значению их ключа*/
+            var studentQuery4 =
+                from student in students
+                group student by student.Last[0] into studentGroup
+                orderby studentGroup.Key
+                select studentGroup;
+            foreach (var groupOfStudents in studentQuery4)
+            {
+                Console.WriteLine(groupOfStudents.Key);
+                foreach (var student in groupOfStudents)
+                {
+                    Console.WriteLine("   {0}, {1}",
+                        student.Last, student.First);
+                }
+            }
+
+            /*Введение идентификаторов с помощью let*/
+            var studentQuery5 =
+                from student in students
+                let totalScore = student.Scores[0] + student.Scores[1] +
+                    student.Scores[2] + student.Scores[3]
+                where totalScore / 4 < student.Scores[0]
+                select student.Last + " " + student.First;
+            foreach (string s in studentQuery5)
+            {
+                Console.WriteLine(s);
+            }
+
+            /*Использование синтаксиса метода в выражении запроса*/
+            var studentQuery6 =
+                from student in students
+                let totalScore = student.Scores[0] + student.Scores[1] +
+                    student.Scores[2] + student.Scores[3]
+                select totalScore;
+
+            double averageScore = studentQuery6.Average();
+            Console.WriteLine("Class average score = {0}", averageScore);
+
+            /*Преобразование или проецирование в предложении select*/
+            IEnumerable<string> studentQuery7 =
+                from student in students
+                where student.Last == "Garcia"
+                select student.First;
+            Console.WriteLine("The Garcias in the class are:");
+            foreach (string s in studentQuery7)
+            {
+                Console.WriteLine(s);
+            }
+
+            /*анонимный тип*/
+            var studentQuery8 =
+                from student in students
+                let x = student.Scores[0] + student.Scores[1] +
+                    student.Scores[2] + student.Scores[3]
+                where x > averageScore
+                select new { id = student.ID, score = x };
+
+            foreach (var item in studentQuery8)
+            {
+                Console.WriteLine("Student ID: {0}, Score: {1}", item.id, item.score);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public class Student
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public int ID { get; set; }
+        public List<int> Scores;
+    }
+}
