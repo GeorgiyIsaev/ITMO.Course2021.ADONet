852b096ee470d0f0638cc98af415347ccd274e3b
Георгий Исаев <georgiyelbaf@gmail.com>
2021-08-02 00:05:16 +0300

    full lab09 ex03

--- Lab09.Ex03.CourseManager/Form1.Designer.cs
+++ Lab09.Ex03.CourseManager/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.departmentList = new System.Windows.Forms.ComboBox();
             this.closeForm = new System.Windows.Forms.Button();
             this.courseGridView = new System.Windows.Forms.DataGridView();
+            this.saveChanges = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,6 +42,7 @@
             this.departmentList.Name = "departmentList";
             this.departmentList.Size = new System.Drawing.Size(208, 21);
             this.departmentList.TabIndex = 0;
+            this.departmentList.SelectedIndexChanged += new System.EventHandler(this.departmentList_SelectedIndexChanged);
             // 
             // closeForm
             // 
@@ -50,6 +52,7 @@
             this.closeForm.TabIndex = 1;
             this.closeForm.Text = "Закрыть";
             this.closeForm.UseVisualStyleBackColor = true;
+            this.closeForm.Click += new System.EventHandler(this.closeForm_Click);
             // 
             // courseGridView
             // 
@@ -59,16 +62,28 @@
             this.courseGridView.Size = new System.Drawing.Size(775, 256);
             this.courseGridView.TabIndex = 2;
             // 
+            // saveChanges
+            // 
+            this.saveChanges.Location = new System.Drawing.Point(227, 41);
+            this.saveChanges.Name = "saveChanges";
+            this.saveChanges.Size = new System.Drawing.Size(208, 23);
+            this.saveChanges.TabIndex = 3;
+            this.saveChanges.Text = "Обновить";
+            this.saveChanges.UseVisualStyleBackColor = true;
+            this.saveChanges.Click += new System.EventHandler(this.saveChanges_Click);
+            // 
             // CourseViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveChanges);
             this.Controls.Add(this.courseGridView);
             this.Controls.Add(this.closeForm);
             this.Controls.Add(this.departmentList);
             this.Name = "CourseViewer";
             this.Text = "Course Viewer";
+            this.Load += new System.EventHandler(this.CourseViewer_Load);
             ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).EndInit();
             this.ResumeLayout(false);
 
@@ -79,6 +94,7 @@
         private System.Windows.Forms.ComboBox departmentList;
         private System.Windows.Forms.Button closeForm;
         private System.Windows.Forms.DataGridView courseGridView;
+        private System.Windows.Forms.Button saveChanges;
     }
 }
 
