4f30bf951d1dd73d31f55787b9adab48abe04f74
Георгий Исаев <georgiyelbaf@gmail.com>
2021-08-03 21:38:28 +0300

    Подключение к БД

---
+++ ITMO.ADONet.ZachetEF.ver2/SampleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.ADONet.Zachet
+{
+    public class SampleContext : DbContext
+    {
+        public SampleContext() : base("MyPetNadzor")
+        { }
+
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<PetType> PetTypes { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
