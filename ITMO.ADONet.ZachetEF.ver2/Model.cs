e0be8255a6a988a8dbc11f14a57061a1709da2ed
Георгий Исаев <georgiyelbaf@gmail.com>
2021-08-03 21:35:05 +0300

    Структура модели

---
+++ ITMO.ADONet.ZachetEF.ver2/Model.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.ADONet.Zachet
+{
+    public class Owner
+    {
+        public int OwnerId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string DocumentNumber { get; set; }
+        public string Telefon { get; set; }
+        public string Email { get; set; }
+    }
+    public class PetType
+    {
+        public int PetTypeId { get; set; }
+        public string Type { get; set; }
+        public string Bbreed { get; set; }
+        public string Characteristic { get; set; }
+        public string DopInfo { get; set; }
+    }
+    public class Pet
+    {
+        public int PetId { get; set; }
+        public PetType PetTypeId { get; set; }
+        public Owner OwnerId { get; set; }
+        public string Name { get; set; }
+        public string DataRegistr { get; set; } 
+    }
+}
